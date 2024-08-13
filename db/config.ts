e384a48e1fdd6743a7fdf7546786f31f7f655dce
Lorech <laurisdedumets@gmail.com>
2024-08-13 22:26:32 +0300

    feat: Define Game table

--- db/config.ts
+++ db/config.ts
@@ -1,5 +1,17 @@
 import { column, defineDb, defineTable } from "astro:db";
 
+const Game = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    name: column.text(),
+    shortName: column.text(),
+    slug: column.text(),
+    logoUrl: column.text(),
+  },
+});
+
 export default defineDb({
-  tables: {},
+  tables: {
+    Game,
+  },
 });
