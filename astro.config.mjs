eff84964fdad5437957458e8d07766cd2813ed54
Lorech <laurisdedumets@gmail.com>
2024-08-13 22:57:15 +0300

    feat: Add Node SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from "astro/config";
-
 import db from "@astrojs/db";
+import node from "@astrojs/node";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [db()],
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
